3f19d8396f2202415f5f74359c2792a1bbfcfe81
azizallaev78@gmail.com <azizallaev78@gmail.com>
2026-05-23 16:08:38 +0500

    Добавьте файлы проекта.

---
+++ TelegramChannelScanner/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading.Tasks;
+using WTelegram;
+using TL;
+
+namespace TelegramChannelScanner
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+
+		}
+	}
+}
