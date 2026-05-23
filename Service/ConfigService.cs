5c46c5038320d0e727c640635a0e77cd104e4388
azizallaev78@gmail.com <azizallaev78@gmail.com>
2026-05-23 17:00:31 +0500

    - Nuget packges added - New class library "Service"

---
+++ Service/ConfigService.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Service
+{
+	public class ConfigService
+	{
+		public IConfigurationRoot ReturnConfigService(string configFilePath) 
+		{
+			return new ConfigurationBuilder()
+			.AddJsonFile(configFilePath)
+			.Build();
+		}
+	}
+}
--- TelegramChannelScanner/Program.cs
+++ TelegramChannelScanner/Program.cs
@@ -1,15 +1,29 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using System;
 using System.Threading.Tasks;
-using WTelegram;
+using Telegram.Bot;
+using Telegram.Bot.Types;
 using TL;
+using WTelegram;
+using Service;
+
 
 namespace TelegramChannelScanner
 {
 	internal class Program
 	{
+		public static readonly ConfigService _configService = new ConfigService();
+
 		static void Main(string[] args)
 		{
+			var config = _configService.ReturnConfigService("appsettings.json");
 
+			string? token = config["Bot:Token"];
+			if (token == null)
+				return;
+
+			var bot = new TelegramBotClient(token);
 		}
+
 	}
 }
